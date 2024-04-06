cec265db795ab83d792bf15064dd8b3e319b6661
9archik <Pochtamedvedya00@mail.ru>
2024-04-06 23:09:09 +0300

    Доделал шапку админа

--- src/components/HeaderAdmin/index.jsx
+++ src/components/HeaderAdmin/index.jsx
@@ -4,27 +4,16 @@ import styles from './styles.module.scss';
 const HeaderAdmin = ({ onClickCreateEvent, onClickPreview }) => {
 	return (
 		<div className={`${styles.container} container`}>
-			<div>
-				<button
-					onClick={() => {
-						if (typeof onClickSearch === 'function') onClickPreview();
-					}}>
-					Предварительный просмотр
-				</button>
-			</div>
-
 			<h2>Новое событие</h2>
 
-			<div>
-				<button
-					onClick={() => {
-						if (typeof onClickCreateEvent === 'function') {
-							onClickCreateEvent();
-						}
-					}}>
-					Создать
-				</button>
-			</div>
+			<button
+				onClick={() => {
+					if (typeof onClickCreateEvent === 'function') {
+						onClickCreateEvent();
+					}
+				}}>
+				Создать
+			</button>
 		</div>
 	);
 };
